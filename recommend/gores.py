8fdc951d1658f1351c11c1099b9fdc362264ff5d
MysteryArc <wanghyuuuuu@163.com>
2023-04-21 15:58:48 +0800

    所有数据插入完成，并可实现利用Gores接口推荐

--- recommend/gores.py
+++ recommend/gores.py
@@ -12,30 +12,83 @@ db = pymysql.connect(
 cursor = db.cursor() #创建游标对象
 
 #用户查询
-sql = "select id from user"
-cursor.execute(sql)
-alluser = cursor.fetchall() #获取全部数据
-userres = []
-for i in alluser:
-    userres.append(
-        {
-            "UserId": str(i[0])
-        }
-    )
+# usersql = "select id from user"
+# cursor.execute(usersql)
+# alluser = cursor.fetchall() #获取全部数据
+# userres = []
+# for i in alluser:
+#     userres.append(
+#         {
+#             "UserId": str(i[0])
+#         }
+#     )
 
 #音乐查询
-artistsres = []
-tags = []
-sql = "select"
+# artistsres = []
+# tags = []
+# artistssql = "select id from artists"
+# cursor.execute(artistssql)
+# allartists = cursor.fetchall()
+# for i in allartists:
+#     tagsql = "select distinct ut.tag_id from artists a join user_tagged ut on a.id = ut.artists_id where a.id = %d" %(i)
+#     cursor.execute(tagsql)
+#     for j in cursor.fetchall():
+#         tags.append(str(j[0]))
+#     artistsres.append(
+#         {
+#             "ItemId": str(i[0]),
+#             "Labels": tags[:]
+#         }
+#     )
+#     tags.clear()
 
+#查询反馈 喜欢、已读（物品Id、用户Id、反馈类型、时间）
+# readfeedback = []
+# likefeedback = []
+# readsql = "select user_id, artists_id, timestamp from user_artists"
+# likesql = "select user_id, artists_id, timestamp from user_like"
+
+# cursor.execute(readsql)
+# allread = cursor.fetchall()
+# for read in allread:
+#     readfeedback.append(
+#         {
+#             "FeedbackType": "read",
+#             "UserId": str(read[0]),
+#             "ItemId": str(read[1]),
+#             "Timestamp": str(read[2])
+#         }
+#     )
+
+# cursor.execute(likesql)
+# alllike = cursor.fetchall()
+# for like in alllike:
+#     likefeedback.append(
+#         {
+#             "FeedbackType": "like",
+#             "UserId": str(like[0]),
+#             "ItemId": str(like[1]),
+#             "Timestamp": str(like[2])
+#         }
+#     )
 
 def main():
     #create
     client = Gorse('http://127.0.0.1:8087', '')
 
-    for i in userres:
-        client.insert_user(i)
+    # 插入用户
+    # for i in userres:
+    #     client.insert_user(i)
 
-    
+    # 插入音乐
+    # for i in artistsres:
+    #     client.insert_item(i)
 
+    # # 插入已读反馈
+    # client.insert_feedbacks(readfeedback)
+
+    # # 插入积极反馈
+    # client.insert_feedbacks(likefeedback)
+
+    re = client.get_recommend("3", n=5)
 main()
