897a40300df2e1ade0fdd72fdee0a6ecde1aa8fb
MysteryArc <wanghyuuuuu@163.com>
2023-04-24 02:11:26 +0800

    添加logo

--- front/musicplayer/src/components/Menu/MenuList.vue
+++ front/musicplayer/src/components/Menu/MenuList.vue
@@ -6,6 +6,9 @@ const menus = useMenu()
 </script>
 
 <template>
+    <div class="logo">
+        <el-image src="src\assets\logo.png"/>
+    </div>
     <div class="layout-menulist" v-for="(item, i) in menus" :key="i">
         <div class="title">{{ item.name }}</div>
         <div class="menus" v-for="menu in item.menus" :key="menu.key">
@@ -15,5 +18,24 @@ const menus = useMenu()
 </template>
 
 <style scoped>
-
+.logo{
+    height: 75px;
+    margin-left: 10px;
+    margin-top: 5px;
+}
+.layout-menulist{
+    margin-top: 10px;
+    margin-left: 30px;
+}
+.title{
+    height: 30px;
+    font-size: 13px;
+    font-weight: 300;
+}
+.menus{
+    height: 30px;
+}
+div{
+    font-family: 'Microsoft YaHei UI', Arial, sans-serif;
+}
 </style>
